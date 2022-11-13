971beae50fe7f69ee233bf04da22a4d435d2793b
Shaker Hossain <73550448+shaker87@users.noreply.github.com>
2022-11-13 23:38:46 +0600

    Bug fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const headerMenu = document.getElementById("header");
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+
 
 
 window.onscroll = function () {
@@ -17,6 +16,7 @@ function scrollFunction() {
 		sn.style.zIndex = "9999"
 		sn.style.paddingTop = '10px'
 		sn.style.paddingBottom = '10px'
+		
 
 	} else {
 		sn.style.position = 'relative'
@@ -27,41 +27,6 @@ function scrollFunction() {
 		sn.style.paddingBottom = '15px'
 	}
 }
-
-
-
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-scrollFunction();
-};
-
-function scrollFunction() {
-if (
-document.body.scrollTop > 20 ||
-document.documentElement.scrollTop > 20
-) {
-mybutton.style.display = "block";
-} else {
-mybutton.style.display = "none";
-}
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
-}
-
-
-
-
-
-
-
-
-
 
 
 
@@ -93,33 +58,47 @@ $(function () {
 		autoplay: true,
 		autoplayTimeout: 5000,
 		autoplayHoverPause: false,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 2
+			},
+			1200: {
+				items: 2
+			}
+		}
 	});
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 	var counters = $(".count-text");
 	var countersQuantity = counters.length;
 	var counter = [];
-  
+
 	for (i = 0; i < countersQuantity; i++) {
-	  counter[i] = parseInt(counters[i].innerHTML);
+		counter[i] = parseInt(counters[i].innerHTML);
 	}
-  
-	var count = function(start, value, id) {
-	  var localStart = start;
-	  setInterval(function() {
-		if (localStart < value) {
-		  localStart++;
-		  counters[id].innerHTML = localStart + "+";
-		}
-	  }, 40);
+
+	var count = function (start, value, id) {
+		var localStart = start;
+		setInterval(function () {
+			if (localStart < value) {
+				localStart++;
+				counters[id].innerHTML = localStart + "+";
+			}
+		}, 40);
 	}
-  
+
 	for (j = 0; j < countersQuantity; j++) {
-	  count(0, counter[j], j);
+		count(0, counter[j], j);
 	}
-  });
+});
 
 
 
